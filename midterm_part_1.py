22b114172df78b4adf6c01e8b7382db19c4828a6
sabaflz <sabaa.feilizadeh@gmail.com>
2024-10-30 23:56:15 -0700

    midterm part 1

--- midterm_part_1.py
+++ midterm_part_1.py
@@ -3,27 +3,16 @@
 # A Menu-Driven Program for De Anza College Food Court
 
 
-# Constants for prices
-PRICES = {
-    "price_1": 5.25,
-    "price_2": 5.75,
-    "price_3": 5.95,
-    "price_4": 5.95,
-    "price_5": 5.95,
-}
+# A list for prices
+PRICES = [5.25, 5.75, 5.95, 5.95, 5.95]
 
-# Cnstants for name of the burgers
-BURGERS = {
-    "burger_1": "De Anza Burger ",
-    "burger_2": "Bacon Cheese   ",
-    "burger_3": "Mushroom Swiss ",
-    "burger_4": "Western Burger ",
-    "burger_5": "Con Cali Burger",
-}
+# A list for name of the burgers
+BURGERS = ["De Anza Burger ", "Bacon Cheese   ",
+           "Mushroom Swiss ", "Western Burger ",
+           "Con Cali Burger"]
 
 # Constant for tax (for staff)
 TAX = 0.09
-
 
 # Constant for the floating point precision
 PRECISION = 2
@@ -36,11 +25,11 @@ def show_menu():
     """
     print("---------------------------------------------")
     print(" << ----- De Anza Food Court Menu ----- >>   \n")
-    print("1.", BURGERS["burger_1"], "................... $", price_1)
-    print("2.", BURGERS[burger_2], "................... $", price_2)
-    print("3.", BURGERS[burger_3], "................... $", price_3)
-    print("4.", BURGERS[burger_4], "................... $", price_4)
-    print("5.", BURGERS[burger_5], "................... $", price_5)
+    print("1.", BURGERS[0], "................... $", PRICES[0])
+    print("2.", BURGERS[1], "................... $", PRICES[1])
+    print("3.", BURGERS[2], "................... $", PRICES[2])
+    print("4.", BURGERS[3], "................... $", PRICES[3])
+    print("5.", BURGERS[4], "................... $", PRICES[4])
     print("6. Exit")
     print("---------------------------------------------")
 
@@ -52,45 +41,57 @@ def get_inputs():
     This function gets the user's order
     """
 
-    # Define a dictionary for the order quantity
-    orders = {
-        "quantity_1": 0,
-        "quantity_2": 0,
-        "quantity_3": 0,
-        "quantity_4": 0,
-        "quantity_5": 0,
-    }
+    # A list for the quantity of each item
+    quantities = [0, 0, 0, 0, 0]
 
-    # Get the order and the quantity from the user (also validate inputs)
-    try:
-        order = int(input("Please enter a number between 1 to 5 to select your order.(or enter 6 to exit):  "))
+    # Get the order and the quantity from the user and validate inputs
+    user_choice = 0
 
-        if order == 6:
-            print("Have a nice day!")
+    while user_choice != 6:
+        print("---------------------------------------------")
+        try:
+            user_choice = int(input("What would you like to order?\n(Enter a number between 1 to 5 to select your order or enter 6 to exit):  "))
             
-        elif order >= 1 and order < 6:
-            choice = "BURGER_" + order
-                
-            quantity = int(input( "How many of", choice, "would you like to order? "))
+            if user_choice == 6:
+                print("Have a nice day!")
 
-            # Set the correct quantity for the order
-            orders["quantity_" + quantity] += 1
+            elif user_choice >= 1 and user_choice < 6:
 
-            # Set the status of the user (student or staff)
-            status = int(input("Are you a student? (enter 1 for yes, 0 for no): "))
-                
-        else:
-            print("Please enter a valid option from the menu!")
-                
-    except:
-        print("Error, please enter a numeric input!")
+                item_index = user_choice - 1
+
+                flag = True
+                while flag:
+                    try:
+                        item_quantity = int(input(f"How many of {BURGERS[item_index]} would you like to order? "))
+
+                        if item_quantity <= 0:
+                            print("Please enter a positive number!")
+
+                        else:
+                            # Set the correct quantity for the order
+                            quantities[item_index] += item_quantity
+
+                            flag = False
+                    except:
+                        print("Please enter a numeric value!")
+                    
+            else:
+                print("Please enter a valid option from the menu!")
+                    
+        except:
+            print("Error, please enter a numeric input!")
         
+    try:
+        # Set the status of the user (student or staff)
+        status = int(input("Are you a staff or a student? (enter 1 for staff, 0 for student): "))
+    except:
+        print("invalid input for status!")
 
-    return orders, status
+    return quantities, status
 
 # --------------------------------------------------------------------------
 
-def compute_bill(orders, status):
+def compute_bill(quantities, status):
     """
     This function computes the total amount the user has to pay
     """
@@ -99,15 +100,15 @@ def compute_bill(orders, status):
     bill_before_tax = 0
 
     for i in range(5):
-        bill_before_tax += orders["quantity_" + str(i)] * (PRICE_ + i)
+        bill_before_tax += quantities[i] * PRICES[i]
 
-    bill_after_tax = bill_before_tax * status * (1 + TAX)
+    bill_after_tax = bill_before_tax + bill_before_tax * status * TAX
 
     return bill_before_tax, bill_after_tax
 
 # --------------------------------------------------------------------------
 
-def print_bill(orders, status, bill_before_tax, bill_after_tax):
+def print_bill(quantities, status, bill_before_tax, bill_after_tax):
     """
     This function prints the total amount the user has to pay
     """
@@ -115,19 +116,15 @@ def print_bill(orders, status, bill_before_tax, bill_after_tax):
     print(" << ----------- Your Bill ----------- >>   \n")
 
     for i in range(5):
-        if orders["quantity_" + i] != 0:
-            
-            print("1.", BURGER_1, "................... $", PRICE_1)
+        if quantities[i] != 0:
+            print(f"{BURGERS[i]} * {quantities[i]}  ................... ${PRICES[i] * quantities[i]}")
     
-    print("1.", BURGER_1, "................... $", PRICE_1)
-    print("2.", BURGER_2, "................... $", PRICE_2)
-    print("3.", BURGER_3, "................... $", PRICE_3)
-    print("4.", BURGER_4, "................... $", PRICE_4)
-    print("5.", BURGER_5, "................... $", PRICE_5)
-    print("6. Exit")
+    print("\n   Bill before tax  ................... $", round(bill_before_tax , PRECISION))
+    print("   Bill after tax  .................... $", round(bill_after_tax , PRECISION))
+    print("   Total  ............................. $", round(bill_after_tax , PRECISION))
+
+
     print("---------------------------------------------")
-    
-    print("Your bill is $", round(bill , PRECISION))
     
 # --------------------------------------------------------------------------
 
@@ -135,11 +132,13 @@ def main():
     
     show_menu()
 
-    orders, status = get_inputs()
+    quantities, status = get_inputs()
 
-    bill_before_tax, bill_after_tax = compute_bill(orders, status)
+    bill_before_tax, bill_after_tax = compute_bill(quantities, status)
 
-    print_bill(orders, status, bill_before_tax, bill_after_tax)
+    # Don't need to show the bill if they didn't order anything
+    if sum(quantities) != 0:
+        print_bill(quantities, status, bill_before_tax, bill_after_tax)
     
 # --------------------------------------------------------------------------
 
