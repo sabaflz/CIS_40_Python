36233405bb0a6f1147c474d8fc266f7f78edce8b
sabaflz <sabaa.feilizadeh@gmail.com>
2024-11-02 20:33:19 -0700

    fix the status question

--- midterm_part_1.py
+++ midterm_part_1.py
@@ -7,8 +7,8 @@
 PRICES = [5.25, 5.75, 5.95, 5.95, 5.95]
 
 # A list for name of the burgers
-BURGERS = ["De Anza Burger ", "Bacon Cheese   ",
-           "Mushroom Swiss ", "Western Burger ",
+BURGERS = ["De Anza Burger", "Bacon Cheese",
+           "Mushroom Swiss", "Western Burger",
            "Con Cali Burger"]
 
 # Constant for tax (for staff)
@@ -25,10 +25,10 @@ def show_menu():
     """
     print("---------------------------------------------")
     print(" << ----- De Anza Food Court Menu ----- >>   \n")
-    print("1.", BURGERS[0], "................... $", PRICES[0])
-    print("2.", BURGERS[1], "................... $", PRICES[1])
-    print("3.", BURGERS[2], "................... $", PRICES[2])
-    print("4.", BURGERS[3], "................... $", PRICES[3])
+    print("1.", BURGERS[0], ".................... $", PRICES[0])
+    print("2.", BURGERS[1], "...................... $", PRICES[1])
+    print("3.", BURGERS[2], ".................... $", PRICES[2])
+    print("4.", BURGERS[3], ".................... $", PRICES[3])
     print("5.", BURGERS[4], "................... $", PRICES[4])
     print("6. Exit")
     print("---------------------------------------------")
@@ -45,47 +45,54 @@ def get_inputs():
     quantities = [0, 0, 0, 0, 0]
 
     # Get the order and the quantity from the user and validate inputs
-    user_choice = 0
+    user_choice = int(input("What would you like to order?\n(Enter a number between 1 to 5 to select your order or enter 6 to exit):  "))
+    if user_choice == 6:
+        status = -1
+        print("*********************************************")
+        print("Have a nice day!")
+        print("*********************************************")
 
-    while user_choice != 6:
-        print("---------------------------------------------")
-        try:
-            user_choice = int(input("What would you like to order?\n(Enter a number between 1 to 5 to select your order or enter 6 to exit):  "))
-            
-            if user_choice == 6:
-                print("Have a nice day!")
+    else:
 
-            elif user_choice >= 1 and user_choice < 6:
-
-                item_index = user_choice - 1
-
-                flag = True
-                while flag:
-                    try:
-                        item_quantity = int(input(f"How many of {BURGERS[item_index]} would you like to order? "))
-
-                        if item_quantity <= 0:
-                            print("Please enter a positive number!")
-
-                        else:
-                            # Set the correct quantity for the order
-                            quantities[item_index] += item_quantity
-
-                            flag = False
-                    except:
-                        print("Please enter a numeric value!")
+        while user_choice != 6:
+            print("---------------------------------------------")
+            try:
+                user_choice = int(input("What would you like to order?\n(Enter a number between 1 to 5 to select your order or enter 6 to exit):  "))
                     
-            else:
-                print("Please enter a valid option from the menu!")
-                    
-        except:
-            print("Error, please enter a numeric input!")
+                if user_choice >= 1 and user_choice < 6:
+
+                    item_index = user_choice - 1
+
+                    flag = True
+                    while flag:
+                        try:
+                            item_quantity = int(input(f"How many of {BURGERS[item_index]} would you like to order? "))
+
+                            if item_quantity <= 0:
+                                print("Please enter a positive number!")
+
+                            else:
+                                # Set the correct quantity for the order
+                                quantities[item_index] += item_quantity
+
+                                flag = False
+                        except:
+                            print("Please enter a numeric value!")
+                        
+                else:
+                    print("Please enter a valid option from the menu!")
+                        
+            except:
+                print("Error, please enter a numeric input!")
         
-    try:
-        # Set the status of the user (student or staff)
-        status = int(input("Are you a staff or a student? (enter 1 for staff, 0 for student): "))
-    except:
-        print("invalid input for status!")
+        try:
+            # Set the status of the user (student or staff)
+            status = int(input("Are you a staff or a student? (enter 1 for staff, 0 for student): "))
+            while status != 1 and status != 0:
+                print("Please enter a valid option for your status!")
+                status = int(input("Are you a staff or a student? (enter 1 for staff, 0 for student): "))
+        except:
+            print("invalid input for status!")
 
     return quantities, status
 
@@ -112,19 +119,19 @@ def print_bill(quantities, status, bill_before_tax, bill_after_tax):
     """
     This function prints the total amount the user has to pay
     """
-    print("---------------------------------------------")
-    print(" << ----------- Your Bill ----------- >>   \n")
+    print("\n*********************************************")
+    print("   << ----------- Your Bill ----------- >>   \n")
 
     for i in range(5):
         if quantities[i] != 0:
             print(f"{BURGERS[i]} * {quantities[i]}  ................... ${PRICES[i] * quantities[i]}")
     
-    print("\n   Bill before tax  ................... $", round(bill_before_tax , PRECISION))
-    print("   Bill after tax  .................... $", round(bill_after_tax , PRECISION))
-    print("   Total  ............................. $", round(bill_after_tax , PRECISION))
+    print("\n   Bill before tax  .................. $", round(bill_before_tax , PRECISION))
+    print("   Bill after tax  ................... $", round(bill_after_tax , PRECISION))
+    print("   Total  ............................ $", round(bill_after_tax , PRECISION))
 
 
-    print("---------------------------------------------")
+    print("*********************************************\n")
     
 # --------------------------------------------------------------------------
 
@@ -136,7 +143,7 @@ def main():
 
     bill_before_tax, bill_after_tax = compute_bill(quantities, status)
 
-    # Don't need to show the bill if they didn't order anything
+    # Don't show the bill if they didn't order anything
     if sum(quantities) != 0:
         print_bill(quantities, status, bill_before_tax, bill_after_tax)
     
@@ -150,8 +157,27 @@ print("Done!")
 
 '''
 Outputs:
+---------------------------------------------
+ << ----- De Anza Food Court Menu ----- >>   
 
+1. De Anza Burger .................... $ 5.25
+2. Bacon Cheese ...................... $ 5.75
+3. Mushroom Swiss .................... $ 5.95
+4. Western Burger .................... $ 5.95
+5. Con Cali Burger ................... $ 5.95
+6. Exit
+---------------------------------------------
+What would you like to order?
+(Enter a number between 1 to 5 to select your order or enter 6 to exit):  6
+*********************************************
+Have a nice day!
+*********************************************
+Done!
 ---------------------------------------------------
+---------------------------------------------------
+---------------------------------------------------
+
+
 
 
 '''
