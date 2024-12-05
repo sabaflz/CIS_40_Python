083a2d1dd8e022ca76bc447bbe02fd12858f851f
sabaflz <sabaa.feilizadeh@gmail.com>
2024-12-05 10:13:50 -0800

    fix comments for ch9

--- ch9.py
+++ ch9.py
@@ -1,29 +1,7 @@
 # Ch.9 (extra credit)
 # Saba Feilizadeh
-# This program creates a table for the Figure Skating Medals
+# This program simulates a bug's movement along a horizontal line
 
-### TODO
-#
-# P9.12 Write a class Bug that models a bug moving along a horizontal line.
-# The bug moves either to the right or left. Initially,
-# the bug moves to the right, but it can turn to change its direction.
-# In each move, its position changes by one unit in the current direction.
-# Provide a constructor def __init__ (self, initialPosition) and methods
-# def turn(self)
-# def move(self)
-# def getPosition(self)
-# Sample usage:
-# bugsy = Bug (10)
-# bugsy.move() # Now the position is 11
-# bugsy.turn()
-# bugsy.move() # Now the position is 10
-
-# Your driver program should construct a bug,
-# make it move and turn a few times, and print the actual and expected positions.
-###
-
-# Constant for the width of the line for the output
-LINE_WIDTH = 50
 # --------------------------------------------------------------------------
 class Bug:
     def __init__(self, initialPosition):
