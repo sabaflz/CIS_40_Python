d62b294c4786afba1dc5d9f13a3c13a1051154c4
sabaflz <sabaa.feilizadeh@gmail.com>
2024-12-05 10:09:18 -0800

    complete ch9

--- ch9.py
+++ ch9.py
@@ -24,51 +24,100 @@
 
 # Constant for the width of the line for the output
 LINE_WIDTH = 50
-
-# List of medals for each country
-# Country,  Gold,  Silver,  Bronze
-canada = [0, 3, 0]
-italy = [0, 0, 1]
-germany = [0, 0, 1]
-japan = [1, 0, 0]
-russia = [3, 1, 1]
-southkorea = [0, 1, 0]
-us = [1, 0, 1]
-
 # --------------------------------------------------------------------------
-def print_table():
-    '''
-    This function prints a table, showing the medal count for each country.
-    '''
-    # Table header
-    print("Gold", "Silver", "Bronze")
+class Bug:
+    def __init__(self, initialPosition):
+        """
+        Initializes the bug's position and direction.
+        The bug moves to the right by default
+        """
+        self.position = initialPosition
+        self.direction = "right"
 
-    # Table content
-    print("Canada", canada)
-    print("Italy", italy)
-    print("Germany",germany)
-    print("Japan", japan)
-    print("Russia",russia)
-    print("South Korea", southkorea)
-    print("United States", us)
+    def turn(self):
+        """
+        Riverses the direction of the bug.
+        If it's moving right, it will start moving left, and vice versa.
+        """
+        if self.direction == "right":
+            self.direction = "left"
+        else:
+            self.direction = "right"
 
-    print("*" * LINE_WIDTH)
-    print("-" * (LINE_WIDTH // 2))
+        print("Turned ...")
+
+    def move(self):
+        """
+        Moves the bug one unit in it's current direction
+        """
+        if self.direction == "right":
+            self.position += 1
+        else:
+            self.position -= 1
+        print("Moved  ...")
+
+    def getPosition(self):
+        """
+        Returns the current position of the bug.
+        """
+        return self.position
 
 # --------------------------------------------------------------------------
 def main():
+    # Create an instance of the Bug class
+    bugsy = Bug(10)
+
+    # Print the start position
+    print(f"Bug's start position: {bugsy.getPosition()}")
     
-    print_table()
-    
+    # Move the bug
+    bugsy.move() # Now the position should be 11
+    print(f"Bug's expected position: 11")
+    print(f"Bug's actual position:   {bugsy.getPosition()}")
+
+    # Turn the bug and move again
+    bugsy.turn()
+    bugsy.move() # Now the position should be 10
+    print(f"Bug's expected position: 10")
+    print(f"Bug's actual position:   {bugsy.getPosition()}")
+
+    # Turn and move a few more times
+    bugsy.turn()
+    bugsy.move() # 11
+    bugsy.move() # 12
+    bugsy.move() # 13
+    bugsy.move() # 14
+    bugsy.move() # 15
+    bugsy.turn()
+    bugsy.move() # 14
+    print(f"Bug's expected position: 14")
+    print(f"Bug's actual position:   {bugsy.getPosition()}")
 
 # --------------------------------------------------------------------------
 main()
-
 print("Done!")
-
 # --------------------------------------------------------------------------
 '''
-Output 1:
+Output:
 
+Bug's start position: 10
+Moved  ...
+Bug's expected position: 11
+Bug's actual position:   11
+Turned ...
+Moved  ...
+Bug's expected position: 10
+Bug's actual position:   10
+Turned ...
+Moved  ...
+Moved  ...
+Moved  ...
+Moved  ...
+Moved  ...
+Turned ...
+Moved  ...
+Bug's expected position: 14
+Bug's actual position:   14
+Done!
 
 '''
